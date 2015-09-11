64908417582005516a8807ecd29697eac84efe9e
Anton Zaytsev <me@antonzaytsev.com>
2015-09-11 22:00:22 +0600

    improved

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :ssh_options, {
 set :log_level, :info #:debug
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 
 set :keep_releases, 5
 
