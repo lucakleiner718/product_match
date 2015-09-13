c9a8bd187204e73ba74d248b4a463f767e2a421d
Anton Zaytsev <me@antonzaytsev.com>
2015-09-13 20:26:09 +0600

    small improvements

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_retailer-products_session'
+Rails.application.config.session_store :cookie_store, key: '_retailer-products_session', domain: {
+  production: 'upc.socialrootdata.com',
+  development: 'localhost'
+}.fetch(Rails.env.to_sym, :all)
