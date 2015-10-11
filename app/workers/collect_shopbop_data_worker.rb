3215989a0934ac9710054e70faf751789d1dea86
Anton Zaytsev <me@antonzaytsev.com>
2015-10-12 03:03:07 +0600

    hotfix for until_executed

--- app/workers/collect_shopbop_data_worker.rb
+++ app/workers/collect_shopbop_data_worker.rb
@@ -1,7 +1,7 @@
 class CollectShopbopDataWorker
 
   include Sidekiq::Worker
-  sidekiq_options unqiue: until_executed
+  sidekiq_options unqiue: :until_executed
 
   def perform
     Import::Shopbop.perform
