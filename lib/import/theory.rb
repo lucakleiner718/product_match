cda735a07b5ec7cc06c0fc9c68552a7d2c42b244
Anton Zaytsev <me@antonzaytsev.com>
2015-10-12 18:35:59 +0600

    added Theory.com import

---
+++ lib/import/theory.rb
@@ -0,0 +1,126 @@
+class Import::Theory < Import::Demandware
+
+  BASEURL = 'https://www.theory.com'
+  SUBDIR =  'Sites-theory_US-Site'
+  NAME =    'theory'
+  PRODUCT_ID_PATTERN = /\/([^\.\/]+)\.html/
+  BRAND_NAME = 'Theory'
+  SOURCE = 'theory.com'
+
+  def self.perform
+    instance = self.new
+    instance.perform
+  end
+
+  def perform
+    [
+      'women-shop-all/womens-shop-all,default,sc.html', 'mens-shop-all/mens-shop-all,default,sc.html',
+      'accessories-womens-shopall1/accessories-womens-shopall1,default,sc.html',
+      'accessories-mens-shopall/accessories-mens-shopall,default,sc.html'
+    ].each do |url_part|
+      puts url_part
+      urls = []
+      url = "#{BASEURL}/#{url_part}"
+      resp = get_request(url)
+      html = Nokogiri::HTML(resp.body)
+
+      products = html.css('#search .product.producttile')
+
+      urls += products.map do |item|
+        url = item.css('.productimage a').first.attr('href').sub(/\?.*/, '')
+        url = "#{BASEURL}#{url}" if url !~ /^http/
+        url
+      end
+
+      urls.uniq!
+
+      urls.each {|u| ProcessImportUrlWorker.perform_async self.class.name, 'process_url', u }
+      puts "spawned #{urls.size} urls"
+      # urls.each {|u| ProcessImportUrlWorker.new.perform self.class.name, 'process_url', u }
+    end
+  end
+
+  def self.process_url url
+    self.new.process_url url
+  end
+
+  def process_url original_url
+    puts "Processing url: #{original_url}"
+    product_id = original_url.match(PRODUCT_ID_PATTERN)[1].split(',').first
+
+    resp = get_request original_url
+    return false if resp.response_code != 200
+
+    url = original_url
+
+    page = resp.body
+    html = Nokogiri::HTML(page)
+
+    # in case we have link with upc instead of inner uuid of product
+    if html.css('link[rel="canonical"]').size == 1
+      url = html.css('link[rel="canonical"]').first.attr('href').sub(/\?.*/, '')
+      product_id = url.match(PRODUCT_ID_PATTERN)[1].split(',').first
+      url = "#{BASEURL}#{url}" if url !~ /^http/
+    end
+
+    product_id_param = product_id
+
+    # brand_name = page.match(/"brand":\s"([^"]+)"/)[1]
+    brand_name = BRAND_NAME# if brand_name.downcase == 'n/a'
+
+    results = []
+
+    product_name = html.css('#pdpMain .productname').first.text.strip
+    category = html.css('#breadcrumb a').inject([]){|ar, el| el.text == 'Home' ? '' : ar << el.text; ar}.join(' > ')
+    # color_param = "dwvar_#{product_id_param}_color"
+    images = html.css("#s7container img").map{|img| img.attr('src')}
+    image_url = images.shift
+
+    data_url = "#{BASEURL}/on/demandware.store/#{SUBDIR}/default/Product-GetVariants?pid=#{product_id}&format=json"
+    data_resp = get_request(data_url)
+    data_resp = data_resp.body.strip.gsub(/inStockDate\:\s\"[^"]+\",/, '').gsub(/(['"])?([a-zA-Z0-9_]+)(['"])?:/, '"\2":')
+    data = JSON.parse(data_resp)
+
+    data['variations']['variants'].each do |v|
+      upc = v['id']
+      price = v['pricing']['standard']
+      price_sale = v['pricing']['sale']
+      color = v['attributes']['color']
+      size = v['attributes']['size']
+      # color_id = k.split('|').inject({}){|obj, el| a = el.split('-'); obj[a[0]] = a[1]; obj}['color']
+      color_url = url
+
+      results << {
+        title: product_name,
+        category: category,
+        price: price,
+        price_sale: price_sale,
+        color: color,
+        size: size,
+        upc: upc,
+        url: color_url,
+        image: image_url,
+        source_id: product_id,
+      }
+    end
+
+    if brand_name.present?
+      brand = Brand.get_by_name(brand_name)
+      unless brand
+        brand = Brand.where(name: BRAND_NAME).first
+        brand.synonyms.push brand_name
+        brand.save if brand.changed?
+      end
+    end
+
+    results.each do |row|
+      product = Product.where(source: SOURCE, source_id: row[:source_id], color: row[:color], size: row[:size]).first_or_initialize
+      product.attributes = row
+      product.brand_id = brand.id
+      product.save
+    end
+
+    results
+  end
+
+end
