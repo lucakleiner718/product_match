5937e5024b43baf7d1c810e35c83d5a03b22c073
Anton Zaytsev <me@antonzaytsev.com>
2015-10-16 13:52:07 +0600

    added Herve Leger brand import (demandware)

--- lib/import/herveleger.rb
+++ lib/import/herveleger.rb
@@ -1,9 +1,9 @@
-class Import::Dkny < Import::Demandware
+class Import::Herveleger < Import::Demandware
 
-  def baseurl; 'http://www.dkny.com'; end
-  def subdir; 'dkny'; end
-  def product_id_pattern; /([A-Z0-9]+)\.html/i; end
-  def brand_name_default; 'DKNY'; end
+  def baseurl; 'http://www.herveleger.com'; end
+  def subdir; 'HerveLeger'; end
+  def product_id_pattern; /([A-Z0-9\-]+)\.html/i; end
+  def brand_name_default; 'Herve Leger'; end
 
   def self.perform
     instance = self.new
@@ -12,22 +12,24 @@ class Import::Dkny < Import::Demandware
 
   def perform
     [
-      'ready-to-wear/women/view-all', 'ready-to-wear/men/view-all',
-      'ready-to-wear/features/dkny-pure', 'ready-to-wear/features/the-new-essentials',
-      'ready-to-wear/features/best-of-black', 'ready-to-wear/features/runway%3A-fall-2015',
-      'ready-to-wear/features/the-coat-shop',
-      'bags/bags/view-all', 'shoes/shoes/view-all', 'accessories/accessories/view-all',
+      'NEW-ARRIVALS/just-in-new-arrivals,default,sc.html', 'THE-PRE-SPRING-COLLECTION/runway-resort,default,sc.html',
+      'EVENT-DRESSING/just-in-event,default,sc.html',
+      'DRESSES/dresses,default,sc.html', 'SIGNATURE/signature,default,sc.html', 'CLOTHING/clothing,default,sc.html',
+      'ACCESSORIES/accessories,default,sc.html', 'FALL-2015/runway-fall,default,sc.html',
+      'PRE-FALL-2015/runway-pre-fall,default,sc.html',
+      'SUMMER-2015/runway-summer,default,sc.html', 'THE-PRE-SPRING-COLLECTION/runway-resort,default,sc.html',
+      'SALE/sale,default,sc.html'
     ].each do |url_part|
       puts url_part
       start = 0
       size = 50
       urls = []
       while true
-        url = "#{baseurl}/#{url_part}/?sz=#{size}&start=#{start}&format=page-element"
+        url = "#{baseurl}/#{url_part}/?sz=#{size}&start=#{start}&format=ajax"
         resp = get_request(url)
         html = Nokogiri::HTML(resp.body)
 
-        products = html.css('a').map{|a| a.attr('href').sub(/\?.*/, '')}.uniq
+        products = html.css('.product-tile a').map{|a| a.attr('href')}.select{|l| l.present?}.map{|a| a.sub(/\?.*/, '')}.uniq
         break if products.size == 0
 
         urls += products
@@ -48,33 +50,48 @@ class Import::Dkny < Import::Demandware
 
   def process_url original_url
     puts "Processing url: #{original_url}"
-    product_id = original_url.match(product_id_pattern)[1]
 
-    resp = get_request("#{baseurl}/#{product_id}.html")
-    return false if resp.response_code != 200
+    # format like: http://www.herveleger.com/Carmen-Woodgrain-Foil-Bandage-Dress/HUN6W073-F6K,default,pd.html"
+    if original_url =~ /\/[a-z0-9\-]+,[a-z\,]+\.html$/i
+      url = original_url
+      product_id = original_url.match(/\/([a-z0-9\-]+),[a-z\,]+\.html$/i)[1]
+      resp = get_request(url)
+    else
+      product_id = original_url.match(product_id_pattern)[1]
 
-    url = resp.last_effective_url
+      resp = get_request("#{baseurl}/#{product_id}.html")
+      return false if resp.response_code != 200
+
+      url = resp.last_effective_url
+    end
 
     page = resp.body
     html = Nokogiri::HTML(page)
 
     # in case we have link with upc instead of inner uuid of product
-    url = html.css('link[rel="canonical"]').first.attr('href') if html.css('link[rel="canonical"]').size == 1
-    product_id = url.match(product_id_pattern)[1]
+    canonical_url = html.css('link[rel="canonical"]').first.attr('href') if html.css('link[rel="canonical"]').size == 1
+    if canonical_url != url
+      product_id = url.match(product_id_pattern)[1]
+      url = "#{baseurl}#{url}" if url !~ /^http/
+    end
+
     product_id_param = product_id
-    url = "#{baseurl}#{url}" if url !~ /^http/
 
     # brand_name = page.match(/"brand":\s"([^"]+)"/)[1]
     brand_name = brand_name_default# if brand_name.downcase == 'n/a'
 
     results = []
-    product_name = html.css('#pdpMain .product-detail .product-name').first.text.strip.sub(/^DKNY\s/, '')
-    category = html.css('.breadcrumbs a').inject([]){|ar, el| el.text == 'Home' ? '' : ar << el.text.strip; ar}.join(' > ')
+    # product_name = html.css('#pdpMain .product-detail .product-name').first.text.strip.sub(/^DKNY\s/, '')
+    product_name = page.match(/app\.page\.setContext\(\{"title":"([^"]+)"/)[1]
+    category = html.css('.breadcrumb a').inject([]){|ar, el| el.text == 'Home' ? '' : ar << el.text.strip; ar}.join(' > ')
 
     color_param = "dwvar_#{product_id_param}_color"
 
     colors = html.css('.attribute .Color li a').inject({}) do |obj, a|
-      color_id = a.attr('href').match(/_color=([^&]+)/)[1]
+      color_id = a.attr('href').match(/_color=([^&]*)/)[1]
+      if color_id.blank?
+        color_id = product_id.match(/-([a-z0-9]+)$/i)[1]
+      end
       begin
         obj[color_id] = JSON.parse(a.attr('data-lgimg'))['url']
       rescue JSON::ParserError => e
