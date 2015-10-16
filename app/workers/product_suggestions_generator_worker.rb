3930daab75971876b783320678ffebdef90e8122
Anton Zaytsev <me@antonzaytsev.com>
2015-10-16 17:28:12 +0600

    updated ProductSuggestionsGeneratorWorker

--- app/workers/product_suggestions_generator_worker.rb
+++ app/workers/product_suggestions_generator_worker.rb
@@ -1,15 +1,13 @@
 class ProductSuggestionsGeneratorWorker
 
   include Sidekiq::Worker
-  sidekiq_options queue: :middle
+  sidekiq_options queue: :middle, unqiue: :until_executed
 
-  def perform *args
+  def perform brand_id, *args
     options = args.extract_options!
     options.symbolize_keys!
 
-    brand = nil
-    brand = Brand.find_by_name(options[:brand]) if options[:brand]
-    brand = Brand.find(options[:brand_id]) if options[:brand_id]
+    brand = Brand.find(brand_id)
 
     delete_exists = options[:delete_exists]
 
@@ -21,13 +19,15 @@ class ProductSuggestionsGeneratorWorker
     end
 
     products = Product.where.not(id: exists_ids).where(source: :shopbop).where(upc: nil)
-    if brand
-      products = products.where(brand_id: brand.id)
-    else
-      products = products.where(brand_id: Brand.in_use.pluck(:id))
-    end
+    products = products.where(brand_id: brand.id)
     products.find_each do |product|
       ProductSuggestionsWorker.perform_async product.id
+    end
+  end
+
+  def self.spawn
+    Brand.in_use.pluck(:id).each do |brand_id|
+      self.perform_async brand_id
     end
   end
 
