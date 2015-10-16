7fe2867efeaff2f1488ba1f6abcd500fb2b65ce4
Anton Zaytsev <me@antonzaytsev.com>
2015-10-16 18:12:19 +0600

    added ability to process products with size for anyahindmarch

--- lib/import/anyahindmarch.rb
+++ lib/import/anyahindmarch.rb
@@ -1,6 +1,7 @@
 class Import::Anyahindmarch < Import::Demandware
 
   def baseurl; 'http://www.anyahindmarch.com'; end
+  def subdir; 'anya_uk'; end
   def product_id_pattern; /(\d{13})\.html/i; end
   def brand_name_default; 'Anya Hindmarch'; end
 
@@ -48,7 +49,13 @@ class Import::Anyahindmarch < Import::Demandware
 
   def process_url original_url
     puts "Processing url: #{original_url}"
-    product_id = original_url.match(product_id_pattern)[1]
+    if original_url =~ product_id_pattern
+      product_id = original_url.match(product_id_pattern)[1]
+      product_id_ean = true
+    else
+      product_id = original_url.match(/-([a-z0-9]+)\.html/i)[1]
+      product_id_ean = false
+    end
 
     resp = get_request("#{baseurl}/#{product_id}.html")
     return false if resp.response_code != 200
@@ -58,44 +65,69 @@ class Import::Anyahindmarch < Import::Demandware
     page = resp.body
     html = Nokogiri::HTML(page)
 
+    return false if html.css('.notfounddesc').size == 1
+
     # in case we have link with upc instead of inner uuid of product
-    canonical_url = html.css('link[rel="canonical"]').first.attr('href') if html.css('link[rel="canonical"]').size == 1
-    if canonical_url && canonical_url != url
-      product_id = url.match(product_id_pattern)[1]
-      url = "#{baseurl}#{url}" if url !~ /^http/
-    end
-    product_id_param = product_id
+    # canonical_url = html.css('link[rel="canonical"]').first.attr('href') if html.css('link[rel="canonical"]').size == 1
+    # if canonical_url && canonical_url != url
+    #   product_id = url.match(product_id_pattern)[1]
+    #   url = "#{baseurl}#{url}" if url !~ /^http/
+    # end
+    # product_id_param = product_id
 
     # brand_name = page.match(/"brand":\s"([^"]+)"/)[1]
     brand_name = brand_name_default# if brand_name.downcase == 'n/a'
 
     results = []
-    product_name = html.css('#pdpMain .productinfo .productname').first.text.strip.sub(/^DKNY\s/, '')
+    product_name = html.css('#pdpMain .productinfo .productname').first.text.strip
     category = html.css('.breadcrumb a').inject([]){|ar, el| el.text == 'Home' ? '' : ar << el.text.strip; ar}.join(' > ')
 
-    pricing = page.match(/"pricing": {"standard": "([^"]+)", "sale": "([^"]+)"/)
-    price = pricing[1].to_f
-    price_sale = pricing[2].to_f
-
-    color = url.match(/\/([^\/]+)-\d+\.html/)[1].gsub('-', ' ')
-
-    ean = product_id
-    if price == 0 && price_sale.present? && price_sale > 0
-      price = price_sale
-      price_sale = nil
-    end
+    color = url.match(/\/([^\/]+)-[a-z0-9]+\.html/i)[1].gsub('-', ' ')
     image_url = page.match(/large:\[\s+\{url: '([^']+)'/)[1]
 
-    results << {
-      title: product_name,
-      category: category,
-      price: price,
-      price_sale: price_sale,
-      color: color,
-      ean: ean,
-      url: url,
-      image: image_url,
-    }
+    if product_id_ean
+      pricing = page.match(/"pricing": {"standard": "([^"]+)", "sale": "([^"]+)"/)
+      price = pricing[1].to_f
+      price_sale = pricing[2].to_f
+
+      ean = product_id
+      if price == 0 && price_sale.present? && price_sale > 0
+        price = price_sale
+        price_sale = nil
+      end
+
+      results << {
+        title: product_name,
+        category: category,
+        price: price,
+        price_sale: price_sale,
+        color: color,
+        ean: ean,
+        url: url,
+        image: image_url,
+      }
+    else
+      variants_url = "#{baseurl}/on/demandware.store/Sites-#{subdir}-Site/en_GB/Product-GetVariants?pid=#{product_id}&format=json"
+      variants = JSON.parse(get_request(variants_url).body)
+      variants['variations']['variants'].each do |variant|
+        ean = variant['id']
+        size = variant['attributes']['size']
+        price = variant['pricing']['standard']
+        price_sale = variant['pricing']['sale']
+
+        results << {
+          title: product_name,
+          category: category,
+          price: price,
+          price_sale: price_sale,
+          color: color,
+          size: size,
+          ean: ean,
+          url: url,
+          image: image_url
+        }
+      end
+    end
 
     if brand_name.present?
       brand = Brand.get_by_name(brand_name)
