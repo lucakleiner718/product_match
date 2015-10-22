e353f48500a640de2ad4468e3b5105b944089d88
Anton Zaytsev <me@antonzaytsev.com>
2015-10-22 11:00:17 +0600

    brand stat fixed

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -57,7 +57,7 @@ class Brand < ActiveRecord::Base
       SELECT count(distinct(product_id)) as amount
       FROM product_selects AS ps
       LEFT JOIN products AS pr ON pr.id=ps.product_id
-      WHERE ps.decision='found' AND pr.brand_id=#{49}
+      WHERE ps.decision='found' AND pr.brand_id=#{Brand.sanitize self.id}
     ").to_a.first['amount'].to_i
 
     # shopbop_matched_size
