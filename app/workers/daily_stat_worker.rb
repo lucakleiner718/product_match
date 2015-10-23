e286086ae1f504c250f0d85a447be51f13cbd2e6
Anton Zaytsev <me@antonzaytsev.com>
2015-10-23 14:14:59 +0600

    improved product suggestion generator worker

--- app/workers/daily_stat_worker.rb
+++ app/workers/daily_stat_worker.rb
@@ -4,15 +4,15 @@ class DailyStatWorker
   sidekiq_options unique: true, queue: :critical
 
   def perform
-    current_date = Date.current
+    date = Date.current
 
     total_size = Product.shopbop.size
-    sa = StatAmount.where(date: current_date, key: 'shopbop_total').first_or_initialize
+    sa = StatAmount.where(date: date, key: 'shopbop_total').first_or_initialize
     sa.value = total_size
     sa.save if sa.changed?
 
     not_matched_size = Product.shopbop.without_upc.size
-    sa = StatAmount.where(date: current_date, key: 'not_matched_size').first_or_initialize
+    sa = StatAmount.where(date: date, key: 'not_matched_size').first_or_initialize
     sa.value = not_matched_size
     sa.save if sa.changed?
   end
--- app/workers/product_suggestions_generator_worker.rb
+++ app/workers/product_suggestions_generator_worker.rb
@@ -12,13 +12,19 @@ class ProductSuggestionsGeneratorWorker
     delete_exists = options[:delete_exists]
 
     if delete_exists && brand
-      ProductSuggestion.where(product_id: Product.shopbop.where(brand_id: brand.id).pluck(:id)).delete_all
-      exists_ids = []
+      if delete_exists.to_sym == :except_green
+        bops_products = Product.shopbop.where(brand_id: brand.id, match: true).pluck(:id)
+        exists_ids = ProductSuggestion.where(product_id: bops_products, percentage: 100).pluck(:product_id).uniq
+        ProductSuggestion.where(product_id: bops_products - exists_ids).delete_all
+      else
+        ProductSuggestion.where(product_id: Product.shopbop.where(brand_id: brand.id).pluck(:id)).delete_all
+        exists_ids = []
+      end
     else
       exists_ids = ProductSuggestion.select('distinct(product_id)').to_a.map(&:product_id)
     end
 
-    products = Product.where.not(id: exists_ids).where(source: :shopbop).where(upc: nil)
+    products = Product.where.not(id: exists_ids).where(source: :shopbop, match: true).where(upc: nil)
     products = products.where(brand_id: brand.id)
     products.find_each do |product|
       ProductSuggestionsWorker.perform_async product.id
