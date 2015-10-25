9eb4d04629fa4330e485f4459a1bb9de0cd39871
Anton Zaytsev <me@antonzaytsev.com>
2015-10-26 04:50:15 +0600

    improved weekly export for shopbop, added new file with products only added for last week

--- app/workers/export_shopbop_worker.rb
+++ app/workers/export_shopbop_worker.rb
@@ -7,14 +7,20 @@ class ExportShopbopWorker
     te = ts.sunday.end_of_day
 
     products_ids = ProductUpc.where('created_at >= ? AND created_at <= ?', ts, te).pluck(:product_id)
-
     csv_string = CSV.generate do |csv|
       Product.where(id: products_ids).each do |product|
         csv << [product.source_id, product.upc]
       end
     end
-
     File.write("public/downloads/shopbop_products_upc-#{te.strftime('%m_%d_%y')}-archive.csv", csv_string)
+
+    products_ids = Product.where('products.created_at >= ? AND products.created_at <= ?', ts, te).joins('RIGHT JOIN product_upcs ON product_upcs.product_id=products.id').pluck(:product_id)
+    csv_string = CSV.generate do |csv|
+      Product.where(id: products_ids).each do |product|
+        csv << [product.source_id, product.upc]
+      end
+    end
+    File.write("public/downloads/shopbop_products_upc.csv", csv_string)
   end
 
 end
