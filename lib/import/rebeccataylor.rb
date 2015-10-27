3e37052fd8a52dd7e52b42f57d37d81d72b7d9f2
Anton Zaytsev <me@antonzaytsev.com>
2015-10-27 16:19:52 +0600

    added Rebeccataylor import

---
+++ lib/import/rebeccataylor.rb
@@ -0,0 +1,91 @@
+class Import::Rebeccataylor < Import::Venda
+
+  def baseurl; 'http://www.rebeccataylor.com'; end
+  def brand_name_default; 'Rebecca Taylor'; end
+
+  def perform
+    [
+      'shop/new-arrivals/icat/rtlatestitems', 'shop/dresses-and-jumpsuits/icat/rtdressesandjumpsuits',
+      'shop/blouses/icat/rtblouses', 'shop/tops-and-tees/icat/rttopsandtees', 'shop/jackets-coats-and-outerwear/icat/rtjackets',
+      'shop/sweaters/icat/rtsweaters', 'shop/pants-shorts-and-skirts/icat/rtbottoms', 'shop/accessories-/icat/rtaccessories',
+      'shop/jewelry/icat/rtjewelry', 'shop/shoes/icat/rtshoes', 'shop/sale/icat/rtsale', 'shop/exclusives/icat/rtfeaturedstyles5',
+      'shop/rebeccas-favorites/icat/rtfeatured12', 'shop/weekend-essentials/icat/rtfeaturedstyles6',
+      'shop/wear-to-work/icat/rtfeaturedstyles1', 'shop/event-dressing/icat/rtfeaturedstyles4',
+    ].each do |url_part|
+      log url_part
+      perpage = 20
+      pagenum = 1
+      urls = []
+      while true
+        url = "#{baseurl}/#{url_part}?setpagenum=#{pagenum}&perpage=#{perpage}&layout=noheaders"
+        resp = get_request(url)
+        next if resp.response_code > 200
+        html = Nokogiri::HTML(resp.body)
+
+        products = html.css('#searchResults .prods li .image a').map{|a| a.attr('href')}.select{|l| l.present?}
+        break if products.size == 0 || (products.size < perpage * 0.9 && products.size == urls.size)
+
+        urls.concat products
+        pagenum += 1
+      end
+
+      urls = urls.map{|url| build_url(url)}.uniq
+
+      spawn_products_urls urls
+    end
+  end
+
+  def process_url url
+    log "Processing url: #{url}"
+    resp = get_request url
+    return false if resp.response_code != 200
+
+    binding.pry
+
+    page = resp.body
+    html = Nokogiri::HTML(page)
+
+    js = html.css('script:contains("product.setAttributeData({")').first.text
+    json_str = "[#{js.scan(/product\.setAttributeData\(({.*})\);/).map{|el| "[#{el.first}]"}.join(',')}]"
+    json = JSON.parse(json_str)
+
+    results = []
+    product_name = html.css('#productdetail-right h1').first.text.strip
+    category = html.css('#breadcrumbs a').inject([]){|ar, el| el.text == 'Home' ? '' : ar << el.text.strip; ar}.join(' > ')
+    gender = 'Female'
+
+    style_code = json[0][1]['atrdssku']
+
+    image_url_mask = "#{baseurl}/content/ebiz/rt/invt/{{style_code}}/{{style_code}}{{color}}setlarge.jpg"
+    colors_images = page.scan(/\.loadImage\("([^"]+)",{\s+setswatch:\s"[^"]+\/#{style_code}(.*)setswatch\.jpg"/).inject({}){|obj, el| obj[el[0]] = el[1]; obj}
+
+    json.each do |row|
+      options = row[1]
+      price = options['atrsell']
+      upc = options['atrsku']
+      color = options['atr1']
+      size = options['atr2']
+
+      image = page.scan(/#{image_url_mask.gsub('{{style_code}}', style_code).sub('{{color}}', colors_images[color])}/).first
+      # page.scan(/#{image_url_mask.gsub('{{style_code}}', style_code).sub('{{color}}', '([^_]+)')}/)
+      # binding.pry unless image
+      raise "No image" unless image
+
+      results << {
+        title: product_name,
+        category: category,
+        price: price,
+        color: color,
+        size: size,
+        upc: upc,
+        url: url,
+        image: image,
+        style_code: style_code,
+        gender: gender
+      }
+    end
+
+    process_results results
+  end
+
+end
