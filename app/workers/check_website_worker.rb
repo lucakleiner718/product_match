717319dfd822feb6c94c03a717ddf700b4bce4e9
Anton Zaytsev <me@antonzaytsev.com>
2015-11-07 10:52:50 +0600

    imrpoved platform detect

--- app/workers/check_website_worker.rb
+++ app/workers/check_website_worker.rb
@@ -3,12 +3,15 @@ class CheckWebsiteWorker
 
   def perform website_id, force: false
     website = Website.find(website_id)
-    return if website.platform.present? && website.platform != 'n/a' && !force
+    if website.platform.blank? || (website.platform == 'n/a' && force)
+      request_url = website.provided_url
+      request_url = website.url if website.url.present? && website.url != 'n/a'
 
-    platform, url = Parser::Platform.detect(website.url || website.provided_url)
-    website.platform = platform || 'n/a'
-    website.url = url || 'n/a'
-    website.save
+      platform, url = Parser::Platform.detect(request_url)
+      website.platform = platform || 'n/a'
+      website.url = url || 'n/a'
+      website.save
+    end
   end
 
   def self.spawn file, force: false
@@ -17,12 +20,12 @@ class CheckWebsiteWorker
     data.each do |url|
       website = websites[url].first
       website = Website.new unless website
-      next if website.platform.present? && website.platform != 'n/a' && !force
+      if website.platform.blank? || (website.platform == 'n/a' && force)
+        website.provided_url = url
+        website.save if website.changed?
 
-      website.provided_url = url
-      website.save if website.changed?
-
-      CheckWebsiteWorker.perform_async website.id, force: force
+        CheckWebsiteWorker.perform_async website.id, force: force
+      end
     end
   end
 end
