ab8d57a4bb68d8eeb5cbaa4c491e7dc6d4c8fd26
Anton Zaytsev <me@antonzaytsev.com>
2015-11-13 00:08:35 +0600

    changed shopbop exported files

--- app/workers/export_shopbop_worker.rb
+++ app/workers/export_shopbop_worker.rb
@@ -13,11 +13,12 @@ class ExportShopbopWorker
     end
     File.write("public/downloads/shopbop_products_upc-#{te.strftime('%m_%d_%y')}-archive.csv", csv_string)
 
-    ts = Time.now.monday
 
-    products_ids = Product.where('products.created_at >= ?', ts)
-                     .joins('RIGHT JOIN product_upcs ON product_upcs.product_id=products.id')
-                     .pluck(:product_id)
+    ts = Time.now.monday
+    products_ids = ProductUpc.where('created_at >= ?', ts).pluck(:product_id)
+    # products_ids = Product.where('products.created_at >= ?', ts)
+    #                  .joins('RIGHT JOIN product_upcs ON product_upcs.product_id=products.id')
+    #                  .pluck(:product_id)
     csv_string = CSV.generate do |csv|
       Product.where(id: products_ids).each do |product|
         csv << [product.source_id, product.upc]
