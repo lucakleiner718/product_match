b80145e10bea3d17e0543364e12e7719c38850c5
Anton Zaytsev <me@antonzaytsev.com>
2015-11-29 23:27:46 +0600

    brand collect worker improved

--- app/workers/brand_collect_data_worker.rb
+++ app/workers/brand_collect_data_worker.rb
@@ -14,9 +14,11 @@ class BrandCollectDataWorker
       case product_source.source_name
         when 'popshops'
           Import::Popshops.perform brand_id: product_source.source_id
+          true
         when 'linksynergy'
           Import::Linksynergy.perform mid: product_source.source_id, daily: true,
             product_source: product_source
+          true
         when 'shopbop'
           Import::Shopbop.perform url: product_source.source_id, update_file: true
           Product.where(source: :shopbop).where('created_at > ?', 12.hours.ago).pluck(:id).each do |pid|
@@ -25,6 +27,7 @@ class BrandCollectDataWorker
           BrandStatWorker.spawn
           DailyStatWorker.perform_async
           ExportShopbopWorker.perform_async 'current'
+          true
         when 'eastdane'
           Import::Eastdane.perform url: product_source.source_id, update_file: true
           Product.where(source: :eastdane).where('created_at > ?', 12.hours.ago).pluck(:id).each do |pid|
@@ -33,10 +36,12 @@ class BrandCollectDataWorker
           BrandStatWorker.spawn
           DailyStatWorker.perform_async
           ExportShopbopWorker.perform_async 'current'
+          true
         when 'website'
           const = product_source.source_id.titleize
           begin
             Module.const_get("Import::#{const}").perform
+
             true
           rescue NameError => e
             if e.message =~ /wrong constant name/
@@ -50,7 +55,12 @@ class BrandCollectDataWorker
           false
       end
 
-    product_source.update_column :collected_at, Time.now if response
+    if response
+      product_source.update_column :collected_at, Time.now if response
+      if product_source.brand.try(:id)
+        ProductSuggestionsWorker.perform_async product_source.brand_id
+      end
+    end
   end
 
   def self.spawn
