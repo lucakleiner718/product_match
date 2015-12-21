709dc5ba1cac8e3f1ea27b698415671743b0fbe1
Anton Zaytsev <me@antonzaytsev.com>
2015-12-21 16:05:38 +0600

    Improved ProductSuggestionsGeneratorWorker

--- app/workers/product_suggestions_generator_worker.rb
+++ app/workers/product_suggestions_generator_worker.rb
@@ -9,14 +9,7 @@ class ProductSuggestionsGeneratorWorker
     options.symbolize_keys!
 
     brand = Brand.find(brand_id)
-
-    exists_ids = ProductSuggestion.select('distinct(product_id)')
-                 .joins("JOIN products on products.id=product_suggestions.product_id
-                         AND products.source IN (#{Product::MATCHED_SOURCES.map{|e| Product.sanitize e}.join(',')})
-                         AND products.brand_id=#{brand_id}").to_a.map(&:product_id)
-
-    products = Product.where.not(id: exists_ids).matching.where(match: true).without_upc
-    products = products.where(brand_id: brand.id)
+    products = Product.matching.where(match: true).without_upc.where(brand_id: brand.id)
 
     in_batch(brand) do
       products.find_each do |product|
