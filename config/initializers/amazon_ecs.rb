4cbddf2a4605dfa4b7c05c4f2f2707f3a9f0aa5d
Anton Zaytsev <me@antonzaytsev.com>
2016-01-28 10:23:43 +0600

    Added ability to provide multiple amazon keys

--- config/initializers/amazon_ecs.rb
+++ config/initializers/amazon_ecs.rb
@@ -1,5 +1,10 @@
+keys = ENV['AMAZON_IMPORT_KEY'].split(',')
+secrets = ENV['AMAZON_IMPORT_SECRET'].split(',')
+tags = ENV['AMAZON_IMPORT_TAG'].split(',')
+index = rand(keys.size)
+
 Amazon::Ecs.configure do |options|
-  options[:AWS_access_key_id] = ENV['AMAZON_IMPORT_KEY']
-  options[:AWS_secret_key] = ENV['AMAZON_IMPORT_SECRET']
-  options[:associate_tag] = ENV['AMAZON_IMPORT_TAG']
+  options[:AWS_access_key_id] = keys[index]
+  options[:AWS_secret_key] = secrets[index]
+  options[:associate_tag] = tags[index]
 end
