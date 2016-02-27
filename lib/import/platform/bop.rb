8ecdccbb21dc6243e4a68ffba05856df061310ea
Anton Zaytsev <me@antonzaytsev.com>
2016-02-28 00:48:42 +0600

    Use file importer module for shared methods

--- lib/import/platform/bop.rb
+++ lib/import/platform/bop.rb
@@ -1,6 +1,5 @@
-require 'xml_parser'
-
 class Import::Platform::Bop < Import::Base
+  include Import::FileImport
 
   def csv_col_sep; ','; end
 
@@ -10,69 +9,10 @@ class Import::Platform::Bop < Import::Base
   end
 
   def perform(url=nil, force=false)
-    raise Exception, "Should be overriden by parent class"
+    raise Exception, "Should be overridden by parent class"
   end
 
   private
-
-  def get_file(url=nil)
-    url ||= default_file
-    extension = url.match(/\.([a-z]+)$/)[1]
-    filename = "tmp/sources/#{self.class.name.match(/::([a-z]+)/i)[1].downcase}.#{extension}"
-    filename_tmp = "tmp/sources/#{self.class.name.match(/::([a-z]+)/i)[1].downcase}_#{Time.now.to_i}.#{extension}"
-
-    @file_updated = false
-    if !File.exists?(filename) || (url_mtime(url) > File.mtime(filename))
-      body = get_request(url).body
-      body.force_encoding('UTF-8')
-      File.write(filename_tmp, body)
-      @file_updated = true
-
-      filename_tmp
-    else
-      filename
-    end
-  end
-
-  def replace_original_tmp_file(filename_tmp)
-    extension = filename_tmp.match(/\.([a-z]+)$/)[1]
-    filename = "tmp/sources/#{self.class.name.match(/::([a-z]+)/i)[1].downcase}.#{extension}"
-    File.delete(filename) if File.exists?(filename)
-    File.rename(filename_tmp, filename)
-  end
-
-  def process_batch(filename)
-    if filename =~ /\.csv$/
-      SmarterCSV.process(filename, col_sep: csv_col_sep, chunk_size: 5_000) do |rows|
-        yield rows
-      end
-    elsif filename =~ /\.xml$/
-      puts "start process file #{filename}"
-      columns = %w|item_group_id id title description product_type google_product_category link image_link
-        condition availability price sale_price brand gender color size material shipping
-        additional_image_link additional_image_link1 additional_image_link2 additional_image_link3
-        additional_image_link4 ean upc|
-
-      batch = []
-      Xml::Parser.factory(filename) do
-        row = {}
-        inside_element 'product' do
-          next if inner_xml.blank?
-          columns.each do |column|
-            for_element column do
-              row[column] = HTMLEntities.new.decode(inner_xml)
-            end
-          end
-        end
-        next if row.blank?
-        batch << row.symbolize_keys!
-
-        if batch.size >= 5_000
-          yield batch
-        end
-      end
-    end
-  end
 
   def process_create(to_create)
     if to_create.size > 0
@@ -89,5 +29,4 @@ class Import::Platform::Bop < Import::Base
       []
     end
   end
-
 end
